f97fa1db5eaad692e81b679ea125926726ee0f01
David Glencross <dglencross@gmail.com>
2018-06-10 17:39:52 +0100

    Change URL-part 'offers' to 'offer' as it really only refers to a singular offer

--- offers/src/main/java/com/offers/OfferResource.java
+++ offers/src/main/java/com/offers/OfferResource.java
@@ -20,7 +20,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-@Path("/offers")
+@Path("/offer")
 public class OfferResource {
 
 	@Context
